6d71b1c97ba8690702cc1b599a81d5e9dc529aa5
david-pilny <93579404+david-pilny@users.noreply.github.com>
2024-02-25 18:49:23 +0100

    WIP

--- src/components/Body/Form/Form.vue
+++ src/components/Body/Form/Form.vue
@@ -7,6 +7,26 @@
 
     <div>
       <form class="form file-form" onkeydown="return event.key != 'Enter';">
+        <div class="form-row flex justify-end">
+          <button type="button" class="cross-button" @click="close">
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
         <!-- single row -->
         <div class="form-row">
           <label for="email" class="form-label">Email (nepovinné)</label>
@@ -77,12 +97,6 @@
         </button>
       </form>
     </div>
-
-    <Modal @close="toggleModal" :modalActive="modalActive">
-      <DefaultModalContent v-if="loading" />
-      <ModalSuccess v-if="success" />
-      <ModalBadTypeFile v-if="badFileType" />
-    </Modal>
   </section>
 </template>
 
@@ -243,3 +257,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.cross-button {
+  @apply text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white;
+}
+</style>
